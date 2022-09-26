a48ae0e6f0882c5c13251ba9b131b999bc96c686
Ilie Burdiniuc <ilya.burdyniuk@yandex.com>
2022-09-26 17:46:28 +0300

    Update settings.py

--- messenger_backend/messenger_backend/settings.py
+++ messenger_backend/messenger_backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=tczumrx($z-jn&p7zfyed1tl*yj70u_n4vu&*v)r$b086)h&c'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
